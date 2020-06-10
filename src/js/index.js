421234262c1b0bbc8e8696bd14a2a4f951a8b8c2
bondardmitry <bondarconstr@gmail.com>
2020-06-10 22:25:32 +0300

    add list

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,11 @@ import Recipe from "./models/Recipe";
 import List from "./models/List";
 import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
+import * as listView from "./views/listView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
 const state = {};
+window.state = state;
 
 const controlSearch = async () => {
     const query = searchView.getInput();
@@ -69,6 +71,25 @@ const controlRecipe = async () => {
     window.addEventListener(event, controlRecipe)
 );
 
+const controlList = async () => {
+    if (!state.list) state.list = new List();
+    state.recipe.ingredients.forEach((ing) => {
+        const item = state.list.addItem(ing.count, ing.unit, ing.ingredient);
+        listView.renderItem(item);
+    });
+};
+
+elements.shopping.addEventListener("click", (e) => {
+    const id = e.target.closest(".shopping__item").dataset.itemid;
+    if (e.target.matches(".shopping__delete, .shopping__delete *")) {
+        state.list.deketeItem(id);
+        listView.deleteItem(id);
+    } else if (e.target.matches(".shopping__count-value")) {
+        const val = parseFloat(e.target.value);
+        state.list.updateCount(id, val);
+    }
+});
+
 elements.recipe.addEventListener("click", (e) => {
     if (e.target.matches(".btn-decrease, .btn-decrease *")) {
         console.log(state.recipe.serving);
@@ -79,6 +100,8 @@ elements.recipe.addEventListener("click", (e) => {
     } else if (e.target.matches(".btn-increase, .btn-increase *")) {
         state.recipe.updateServings("inc");
         recipeView.updateServingsIngredients(state.recipe);
+    } else if (e.target.matches(".recipe__btn--add, .recipe__btn--add *")) {
+        controlList();
     }
 });
 
